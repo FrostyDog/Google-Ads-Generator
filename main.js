685b755ba64d6c29eea9c55ff1966a0071c7291a
FrostyDogLap <aleks.gubskyy@gmail.com>
2019-03-26 01:54:21 +0100

    Headline generation logic added
    Fixed the headline error

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ location: "x";
 var discounts
 var discountPercent
 var buy1Get2
-var Delivery// fast or free
+var delivery// fast or free
 var ecoFriendly = 1;
 var catalogue = 1;// or single product
 
@@ -35,7 +35,7 @@ var catalogue = 1;// or single product
 // var deferentiate= [
 //     catalogue,
 //     ecoFriendly,
-//     Delivery,
+//     delivery,
 //     buy1Get2,
 //     discounts,
 // ] 
@@ -64,40 +64,62 @@ var featureComponents =[
 
 // everything in the if () statement for checking length
 
-function builtHeadline() {
+function builtHeadlines() {
 
-    var hlOptions = ["Buy " + keyWords.product + " online",
+    var hlOptions = [
+        "Buy " + keyWords.product + " online",
 
-    "Online catalogue" + keyWords.product,
+        "Online catalogue" + keyWords.product,
 
-    "Buy " + keyWords.product + "from " + keyWords.companyName]
+        "Buy " + keyWords.product + "from " + keyWords.companyName,
+    ]
 
     for (i = 0; i < hlOptions.length; i++) {
-        // console.log (hlOptions[i]);
+     console.log (hlOptions[i]);
     }
+
+    // Selection the needed lines by class
+    var hlImport = document.getElementsByClassName("hd");
+
+
+
+    // pushing the needed strings in the table  -- (selecting all empty space in the table by selector and replace it with strings)
+    for (i = 0; i < hlOptions.length; i++) {
+        hlImport[i].innerHTML = hlOptions[i];
+    }
+
 
 }
 
-builtHeadline()
+builtHeadlines();
 
 
 // Combain Descriptions - also checking itriggered componetns and adding themon the fly - need to add other triggers //
 
-function builtDescription() {
+function builtDescriptions() {
 
     var descrOptions = [
 
         "Best quality " + keyWords.product + " suitable for everyone. Make your order right now!",
+        "Quality " + keyWords.product + " for most demanding people. Dont wait and order right now!",
     ]
 
     if (ecoFriendly == true) {
-
         descrOptions.push("Eco-friendly " + keyWords.product + " for you and your family. Order now!");
     }
 
     if (catalogue == true) {
         descrOptions.push("Full catalogue of " + keyWords.product + " Choose you favorite and order now");
     }
+
+    if (delivery == true) {
+        descrOptions.push("Fast and secure delivery. Make your order and get your " + keyWords.product + " right to your home");
+    }
+
+    if (buy1Get2 == true) {
+        descrOptions.push("Famous buy 1 get 2 " + keyWords.product + " without any additional costs. Hurry up and place an order!");
+    }
+
 
     // for (i = 0; i < descrOptions.length; i++) {
     //     console.log (descrOptions[i]);
@@ -112,8 +134,6 @@ function builtDescription() {
     }
 
 
-    console.log(descrImport)
-
 }
 
-builtDescription();
+builtDescriptions();
