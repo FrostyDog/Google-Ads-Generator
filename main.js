97729f00eedc2e805a404d67581b6c82c72fd05c
FrostyDogLap <aleks.gubskyy@gmail.com>
2019-04-01 02:25:24 +0200

    Length cheker + 2 type of Headlines
    Implemented a length cheker in headlines functions.
    Devided the headlines into HL 1 and HL 2-3. Devided the content.
    
    ** To do** Probably devide describtions into 2 parts and add content.
    
    ** Strange** loop is repeating itself a lot. But works anyways.

--- main.js
+++ main.js
@@ -44,38 +44,38 @@ function createTableRow() {
 
 
     //capturing number of ads
-    
+
     var numberAds = numberAdsTake.value;
 
 
     var rw = document.getElementsByTagName("tr").length
     console.log(rw)
 
-    for (rw; rw <= numberAds; rw++) { 
+    for (rw; rw <= numberAds; rw++) {
 
-    var table = document.getElementById("table");
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell3 = row.insertCell(2);
-    cell4 = row.insertCell(3);
-    cell5 = row.insertCell(4);
-    cell6 = row.insertCell(5);
-    cell7 = row.insertCell(6);
-    cell8 = row.insertCell(7);
-    cell9 = row.insertCell(8);
-    cell10 = row.insertCell(9);
-    cell11 = row.insertCell(10);
+        var table = document.getElementById("table");
+        var row = table.insertRow(-1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        cell3 = row.insertCell(2);
+        cell4 = row.insertCell(3);
+        cell5 = row.insertCell(4);
+        cell6 = row.insertCell(5);
+        cell7 = row.insertCell(6);
+        cell8 = row.insertCell(7);
+        cell9 = row.insertCell(8);
+        cell10 = row.insertCell(9);
+        cell11 = row.insertCell(10);
 
-    cell6.setAttribute("class","hd1")
-    cell5.setAttribute("class","hd")
-    cell4.setAttribute("class","hd")
-    cell7.setAttribute("class","descr")
-    cell8.setAttribute("class","descr")
-    cell9.setAttribute("class","path1")
-    cell10.setAttribute("class","path2")
-    cell11.setAttribute("class","final-url")
-    
+        cell4.setAttribute("class", "hd1")
+        cell5.setAttribute("class", "hd")
+        cell6.setAttribute("class", "hd")
+        cell7.setAttribute("class", "descr")
+        cell8.setAttribute("class", "descr")
+        cell9.setAttribute("class", "path1")
+        cell10.setAttribute("class", "path2")
+        cell11.setAttribute("class", "final-url")
+
 
     }
 }
@@ -88,7 +88,7 @@ location: "x";
 
 var discounts = document.getElementById("trigger-discounts").value
 // var discountPercent = document.getElementById("discount-number").value
-var buy1Get2 = 1 
+var buy1Get2 = 1
 var delivery = document.getElementById("trigger-delivery").value
 var ecoFriendly = document.getElementById("trigger-eco-friendly").value
 var catalogue = document.getElementById("trigger-catalogue").value
@@ -98,25 +98,44 @@ var catalogue = document.getElementById("trigger-catalogue").value
 
 function builtHeadlines() {
     // First and the most important one
-    var hl1Options = [
-        "Buy " + keyWords.product + " online",
+    var hl1Options = []
+    // rest of the Hedlines
+    var hl23Options = []
 
-        "Online catalogue" + keyWords.product,
-
-        "Buy " + keyWords.product + "from " + keyWords.companyName,
-    ]
-// rest of the Hedlines
-    var hl23Options = [
-    ]
-
-    Headlines()
+    HeadlinesRawCheck()
+    console.log(hl23Options)
     function HeadlinesRawCheck() {
-    h1RawCheck = []
-    hl23RawCheck = []
 
+        h1RawCheck = [
+            "Buy " + keyWords.product + " online",
+
+            "Catalogue of " + keyWords.product,
     
-    }
+            "Buy " + keyWords.product + " from " + keyWords.companyName,
+        ]
 
+        hl23RawCheck = [
+            "Great discounts!",
+            "Don't miss the opportinity!",
+            keyWords.companyName,
+            "Fast Delivery"
+        ]
+
+
+        for (i = 0; i < h1RawCheck.length; i++) {
+            if (h1RawCheck[i].length <= 30) {
+                hl1Options.push(h1RawCheck[i])
+            }
+
+        for (x = 0; x < hl23RawCheck.length; x++) {
+             if (hl23RawCheck[x].length <= 30) {
+                hl23Options.push(hl23RawCheck[x])
+                console.log(hl23RawCheck[x])
+                }
+
+
+            }
+        }}
 
     // Selection the needed lines by class
     var hl1Import = document.getElementsByClassName("hd1");
@@ -129,95 +148,94 @@ function builtHeadlines() {
     }
 
     for (i = 0; i < (numberAds.value * 2); i++) {
-        var random = Math.floor(Math.random() * hl23Options.length)
-        hl23Import[i].innerHTML = hl23Options[random];
+        var random12 = Math.floor(Math.random() * hl23Options.length)
+        hl23Import[i].innerHTML = hl23Options[random12];
     }
 
 
-}
-
-
-// Combain Descriptions - also checking itriggered componetns and adding themon the fly - need to add other triggers //
-
-function builtDescriptions() {
-
-    var descrOptions = []
-    descrRawCheck()
-
-    // Function that checks Raw Descriptions length and if it is okay push it to descrOptions.
-    // if statement first push option to raw so the cycle would    
-    function descrRawCheck() {
-
-       var descrOptionsRaw = [
-            "Best quality " + keyWords.product + " suitable for everyone. Make your order right now!",
-            "Quality " + keyWords.product + " for most demanding people. Dont wait and order right now!",
-            
-        ]     
-        
-        if (ecoFriendly == "on") {
-            descrOptionsRaw.push("Eco-friendly " + keyWords.product + " for you and your family. Order now.");
-        }
-    
-        if (catalogue == "on") {
-            descrOptionsRaw.push("Full catalogue of " + keyWords.product + " Choose you favorite and order now.");
-        }
-    
-        if (delivery == "on") {
-            descrOptionsRaw.push("Fast and secure delivery. Make your order and get your " + keyWords.product + " right to your home.");
-        }
-    
-        if (buy1Get2 == "on") {
-            descrOptionsRaw.push("Famous buy 1 get 2 " + keyWords.product + " without any additional costs. Hurry up and place an order!");
         }
 
-        for (x = 0; x < descrOptionsRaw.length; x++) {
-            if (descrOptionsRaw[x].length <= 90) {
-                descrOptions.push(descrOptionsRaw[x])
+
+        // Combain Descriptions - also checking itriggered componetns and adding themon the fly - need to add other triggers //
+
+        function builtDescriptions() {
+
+            var descrOptions = []
+            descrRawCheck()
+
+            // Function that checks Raw Descriptions length and if it is okay push it to descrOptions.
+            // if statement first push option to raw so the cycle would    
+            function descrRawCheck() {
+
+                var descrOptionsRaw = [
+                    "Best quality " + keyWords.product + " suitable for everyone. Make your order right now!",
+                    "Quality " + keyWords.product + " for most demanding people. Dont wait and order right now!",
+
+                ]
+
+                if (ecoFriendly == "on") {
+                    descrOptionsRaw.push("Eco-friendly " + keyWords.product + " for you and your family. Order now.");
+                }
+
+                if (catalogue == "on") {
+                    descrOptionsRaw.push("Full catalogue of " + keyWords.product + " Choose you favorite and order now.");
+                }
+
+                if (delivery == "on") {
+                    descrOptionsRaw.push("Fast and secure delivery. Make your order and get your " + keyWords.product + " right to your home.");
+                }
+
+                if (buy1Get2 == "on") {
+                    descrOptionsRaw.push("Famous buy 1 get 2 " + keyWords.product + " without any additional costs. Hurry up and place an order!");
+                }
+
+                for (x = 0; x < descrOptionsRaw.length; x++) {
+                    if (descrOptionsRaw[x].length <= 90) {
+                        descrOptions.push(descrOptionsRaw[x])
+                    }
+                }
+
+            }
+
+            // if (discounts == "on") {
+            //     discounts = discountPercent; // number have to be provide by user
+            //     featureComponents.push("Up to " + discounts + "% off.");
+            // }
+
+
+            // Selection the needed lines by class
+            var descrImport = document.getElementsByClassName("descr");
+
+            // pushing the needed strings in the table  -- (selecting all empty space in the table by selector and replace it with strings)
+            for (i = 0; i < (numberAds.value * 2); i++) {
+                var random = Math.floor(Math.random() * descrOptions.length)
+                descrImport[i].innerHTML = descrOptions[random];
+            }
+
+        }
+
+        // createing new line in table that is needed.
+
+        function builtPathsAndUrls() {
+
+            var finalUrls = document.getElementsByClassName("final-url");
+
+            for (i = 0; i < numberAdsTake.value; i++) {
+                finalUrls[i].textContent = keyWords.finalUrl;
+            }
+
+            var paths1 = document.getElementsByClassName("path1");
+
+            for (i = 0; i < numberAds.value; i++) {
+                paths1[i].innerHTML = keyWords.product;
+            }
+
+            var paths2 = document.getElementsByClassName("path2");
+
+            for (i = 0; i < numberAds.value; i++) {
+                paths2[i].innerHTML = keyWords.companyName;
             }
         }
-        
-    }
-
-    // if (discounts == "on") {
-//     discounts = discountPercent; // number have to be provide by user
-//     featureComponents.push("Up to " + discounts + "% off.");
-// }
-
-
-    // Selection the needed lines by class
-    var descrImport = document.getElementsByClassName("descr");
-
-    // pushing the needed strings in the table  -- (selecting all empty space in the table by selector and replace it with strings)
-    for (i = 0; i < (numberAds.value * 2); i++) {
-        var random = Math.floor(Math.random() * descrOptions.length)
-        descrImport[i].innerHTML = descrOptions[random];
-    }
-
-}
-
-// createing new line in table that is needed.
-
-function builtPathsAndUrls() {
-
-    var finalUrls = document.getElementsByClassName("final-url");
-
-    for (i = 0; i < numberAdsTake.value; i++ ) {
-    finalUrls[i].textContent = keyWords.finalUrl;
-    }
-
-    var paths1 = document.getElementsByClassName("path1");
-    console.log(paths1)
-
-    for (i = 0; i < numberAds.value; i++ ) {
-    paths1[i].innerHTML = keyWords.product;
-    }
-
-    var paths2 = document.getElementsByClassName("path2");
-
-    for (i = 0; i < numberAds.value; i++ ) {
-    paths2[i].innerHTML = keyWords.companyName;
-    }
-}
 
 
 
